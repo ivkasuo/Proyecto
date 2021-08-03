b815f1a6105dc898f39b6fa1c1c8939ef2acfd19
Cesar <cesaradrianer2@gmail.com>
2021-08-02 23:17:43 -0500

    Validación de campos básica

--- js/posts.js
+++ js/posts.js
@@ -3,21 +3,26 @@ let postArea = document.getElementById("feed");
 let botonPost = document.getElementById("post_button");
 let post = document.getElementById("post_textarea");
 let nombre = document.getElementById("nombre");
+let campos = document.querySelectorAll(".needs-validation");
+let form = document.getElementById("post_form");
 
-//Función que postea cuando clickeas al botón "Publicar"
-botonPost.addEventListener('click', function() {   
-    postArea.insertAdjacentHTML('beforeend', 
-    `<div class = "card mt-2" style = "width: 18rem;">
-        <div class = "card-body">
-            <div class = "card-title">
-                <strong>${nombre.value} dice:</strong>
-            </div>
-            <hr>
-            <p class = "card-text ms-3">${post.value}</p>
-            <hr>
-            <button class = "btn btn-light">Like</button>
-            <button class = "btn btn-light">Comentar</button>
-        </div> 
-     </div>`
-     )
+form.addEventListener("submit", (e) => {
+    if (nombre.value == "" && post.value == "") {
+        e.preventDefault();
+    } else {
+        postArea.insertAdjacentHTML('beforeend', 
+            `<div class = "card mt-2" style = "max-width: 456px;">
+                <div class = "card-body">
+                    <div class = "card-title">
+                        <strong>${nombre.value} dice:</strong>
+                    </div>
+                    <hr>
+                    <p class = "card-text ms-3">${post.value}</p>
+                    <hr>
+                    <button class = "btn btn-light">Like</button>
+                    <button class = "btn btn-light">Comentar</button>
+                </div> 
+            </div>`
+            )
+    } 
 })
